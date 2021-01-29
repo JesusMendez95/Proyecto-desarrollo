d207d57c340cfe911fbb24afcb41a71efb3e0e29
Jesus Mendez <Mendezpinedaj@gmail.com>
2021-01-29 11:18:08 -0500

    Datos, scripts y recursos

--- Limpieza datos noticias.py
+++ Limpieza datos noticias.py
@@ -1,56 +1,109 @@
 import pandas as pd
 import datetime
+import unidecode
 import re
+from textblob import TextBlob
+from wordcloud import WordCloud
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk import word_tokenize
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
-noticias_republica = pd.read_excel('larepublica raw data.xlsx')
+noticias_esp = pd.read_excel('larepublica raw data.xlsx')
 
+noticias_esp = noticias_esp.dropna()  # Eliminar filas vacias
+noticias_esp = noticias_esp.drop_duplicates()  # Eliminar duplicados
 
-noticias_republica = noticias_republica.dropna()  # Eliminar filas vacias
-noticias_republica = noticias_republica.drop_duplicates()  # Eliminar duplicados
-
-noticias_republica['Noticia'] = noticias_republica['Title'] + '. ' + noticias_republica[
+noticias_esp['Noticia'] = noticias_esp['Title'] + '. ' + noticias_esp[
     'Text']  # concatenar las columnas Title y Text dentro de una sola, Noticia.
 
+noticias_esp = noticias_esp.drop(['Title', 'Text'], 1)  # Descartar las columnas sobrantes
 
-noticias_republica = noticias_republica.drop(['Title', 'Text'], 1)  # Descartar las columnas sobrantes
-
-
-noticias_republica.rename(columns={'Pub_Time': 'Fecha'}, inplace=True)  # Cambiar nombre de la columna fecha
+noticias_esp.rename(columns={'Pub_Time': 'Fecha'}, inplace=True)  # Cambiar nombre de la columna fecha
 
 
 def ajustar_fecha(date):
+    """Función para transformar la fecha al formato estandar .
+
+    Args:
+        date (str): Fecha no estructurada ej: jueves, 3 de enero de 2013
+
+    Returns:
+        fecha_corregida (str): Fecha corregida en formato estandar ej: 2013-01-03
+
+"""
+
     dic = {'enero': '01', 'febrero': '02', 'marzo': '03', 'abril': '04', 'mayo': '05', 'junio': '06', 'julio': '07',
-           'agosto': '08', 'septiembre': '09', 'octubre': '10', 'noviembre': '11', 'diciembre': '12'}
+           'agosto': '08', 'septiembre': '09', 'octubre': '10', 'noviembre': '11', 'diciembre': '12'}  # Diccionario
+    # para convertir el mes de texto a numero, en formato str
 
-    fecha_corregida = []
+    fecha_corregida = []  # Output en lista
 
-    fecha = date.split()
+    fecha = date.split()  # Dividir con el delimitador " " cada palabra que conforma la celda Fecha, formando un objeto lista
 
-    del fecha[0]
-    del fecha[1]
-    del fecha[2]
+    del fecha[0]  # Depurar día de la semana "lunes..."
+    del fecha[1]  # Depurar primera preposicion "de"
+    del fecha[2]  # Depurar segunda preposicion "de"
 
-    fecha_corregida.append(fecha[2])
+    fecha_corregida.append(fecha[2])  # Añade el año a la variable de salida
 
     if fecha[1] in dic.keys():
-        fecha_corregida.append(dic[fecha[1]])
+        fecha_corregida.append(dic[fecha[1]])  # Si el segundo elemento de la variable de entrada (convertida a
+        # lista), esta dentro de las llaves del diccionario dic. Añade el mes a la variable de salida
 
     if len(fecha[0]) == 1:
-        fecha_corregida.append('0' + fecha[0])
+        fecha_corregida.append('0' + fecha[0])  # Se añade un 0 al dia del mes, ej 2013-01-5 -> 2013-01-05
     else:
-        fecha_corregida.append(fecha[0])
+        fecha_corregida.append(fecha[0])  # Si el día tiene 2 digitos, no se altera
 
-    fecha_corregida = '-'.join(fecha_corregida)
+    fecha_corregida = '-'.join(fecha_corregida)  # Se convierte la variable de lista a string, con separador "-" ej:
+    # ['2013', '01', '15'] -> '2013-01-15
 
     return fecha_corregida
 
 
-fechas = pd.Series(map(ajustar_fecha, noticias_republica['Fecha']))
-noticias_republica['Fecha'] = fechas
-
+fechas = pd.Series(map(ajustar_fecha, noticias_esp['Fecha']))  # Convertir el iterable map a una serie de pandas
+noticias_esp['Fecha'] = fechas  # llevar al DataFrame los datos de fecha modificados
 
 # Editando la columna Noticia
 
+# Limpieza con el texto traducido al ingles
+test_text = noticias_esp['Noticia']
 
+
+def eliminar_espacios(texto):
+    pattern = re.compile(r'\s+')
+    texto_sin_espacios = re.sub(pattern, ' ', texto)
+    return texto_sin_espacios
+
+
+t = pd.Series(map(eliminar_espacios, test_text))
+
+"t.to_excel('noticias larepublica.xlsx', encoding='utf-8')"
+
+noticias_columna_ing = pd.read_excel("noticias larepublica.xlsx", sheet_name='english', header=None)
+
+noticias_ing = pd.DataFrame(noticias_esp['Fecha'])
+noticias_ing['Noticias'] = noticias_columna_ing
+
+
+def limpieza_general(texto):
+    texto = texto.lower()  # Establecer todo como minuscula
+
+    texto = re.sub(r'\s+', ' ', texto)  # Quitar espacios y entre lineas
+
+    texto = re.sub('[^\w\s]', '', texto)  # Eliminar simbolos y puntuaciones
+
+    texto = re.sub(r'\w*\d\w*', '', texto)  # Eliminnar numeros y cadenas con numeros
+
+    texto = unidecode.unidecode(texto)  # Eliminar diacriticos y ñs
+
+def tokenizacion_unigrams(texto):
+
+
+def tokenizacion_bigrams(texto):
 
--- Limpieza y acomodacion datos financieros.py
+++ Limpieza y acomodacion datos financieros.py
@@ -1,43 +1,132 @@
-
 # Ajuste datos financieros
 
 
 import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.arima_model import ARIMA
+from pandas.plotting import autocorrelation_plot
+import numpy as np
+from statsmodels.tsa.stattools import adfuller
+from pmdarima import model_selection
+import pmdarima as pm
+import technical_indicators_lib
+
 pd.set_option('display.max_rows', 1000)
-pd.set_option('display.max_columns', 1000)  # Setup para ampliar visualización de datos (en este caso DataFrame) en el IDE
+pd.set_option('display.max_columns',
+              1000)  # Setup para ampliar visualización de datos (en este caso DataFrame) en el IDE
 pd.set_option('display.width', 1000)
 
-
-ecopetrol_df = pd.read_excel("Ecopetrol 2013-2019 raw data.xls")  # Importar datos financieros como Dataframe, (previamente convertidos de
+ecopetrol_df = pd.read_excel(
+    "Ecopetrol 2013-2019 raw data.xls")  # Importar datos financieros como Dataframe, (previamente convertidos de
 # Formato xls a csv) usando pandas
 
 
-
-keep_columns = ['fecha', 'Volumen', 'Precio Cierre', 'Precio Mayor', 'Precio Menor', 'Variacion Absoluta']   # Se
-# Establecen las columnas que se van a usar,"Cantidad de transacciones" y "Precio Medio" se descartan
-
-
-ecopetrol_df['Precio Apertura'] = ecopetrol_df['Precio Cierre'] + ecopetrol_df['Variacion Absoluta']
+ecopetrol_df['Precio Apertura'] = ecopetrol_df['Precio Cierre'] - ecopetrol_df['Variacion Absoluta']
 # Se calcula el valor "Open" de la accion
-
 
 
 columns_fixed = ['fecha', 'Precio Apertura', 'Precio Mayor', 'Precio Menor', 'Precio Cierre', 'Volumen']
 ecopetrol_df = ecopetrol_df[columns_fixed]
+
+ecopetrol_df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
 # Orden de las columnas de datos, según formato requerido
 
+ecopetrol_df['date'] = pd.to_datetime(ecopetrol_df['date'], '%Y-%m-%d')
+
+
+def df_to_float(df):
+    for i in range(len(df.columns)):
+        if i == 0:
+            continue
+        df.iloc[:, i] = pd.to_numeric(df.iloc[:, i], downcast='float')
+
+
+df_to_float(ecopetrol_df)
+
+ecopetrol_df = ecopetrol_df.set_index('date')
+
+print(ecopetrol_df.head())
+
+df_p_cierre = pd.DataFrame(ecopetrol_df['close'], columns=['close'])
+
+# Graficar serie
+
+"""plt.plot(serie_p_cierre.index, serie_p_cierre)
+plt.show()"""
+
+
+# Notas La hipótesis nula de Augmented Dickey-Fuller es que existe una raíz unitaria, con la alternativa de que no
+# existe una raíz unitaria. Si el valor p está por encima de un tamaño crítico, no podemos rechazar que haya una raíz
+# unitaria. Los valores p se obtienen mediante la aproximación de la superficie de regresión de MacKinnon 1994,
+# pero utilizando las tablas actualizadas de 2010. Si el valor p está cerca de ser significativo, entonces los
+# valores críticos deben usarse para juzgar si rechazar el nulo. La opción autolag y maxlag se describen en Greene.
+
+# Determinar si la serie es estacionaria
+
+def test_estacionariedad(serie):
+    # Metodo gráfico, Medidas estadisticas moviles:
+    media_movil = serie.rolling(window=10).mean()
+    std_movil = serie.rolling(window=10).std()
+
+    # Graficar estadisticas moviles:
+
+    original = plt.plot(serie, color='blue', label='Original')
+    media = plt.plot(media_movil, color='red', label='Media Movil')
+    desviacion = plt.plot(std_movil, color='black', label='Desviación Estantar Movil')
+    plt.legend(loc='best')
+    plt.title('Estadisticas Moviles')
+
+    # Graficar Funcion Autocorrelación
+    plot_acf(serie)
+
+    # Graficar Función Autocorrelación Parcial
+    plot_pacf(serie)
+    plt.show()
+
+    # Test Augmented Dickey-Fuller:
+    print('Resultados del Test ADK:')
+    adk_test = adfuller(serie, autolag='AIC')
+    resultado_adk = pd.Series(adk_test[0:4], index=['Test Estadistico', 'p-value', '#Rezagos usados',
+                                                    'Numero de Observaciones Usadas'])
+    for key, value in adk_test[4].items():
+        resultado_adk['Valor critico {}%'.format(key)] = value
+    print(resultado_adk)
+    if adk_test[1] >= 0.05:
+        print('Se rechaza Ho, hay evidencia significativa de que la serie no tiene raiz unitaria')
+    else:
+        print('No se rechaza Ho, hay evidencia significativa de que la serie  tiene raiz unitaria')
+
+
+# Diferenciacion
+
+
+"""s_p_cierre_diff1 = serie_p_cierre - serie_p_cierre.shift(1)
+s_p_cierre_diff1 = s_p_cierre_diff1.dropna()
+
+train, test = model_selection.train_test_split(serie_p_cierre.iloc[0:200,0], train_size=50)
+
+arima = pm.auto_arima(train, trace=True)
+
+
+# Plot actual test vs. forecasts:
+x = np.arange(test.shape[0])
+plt.plot(x, test)
+plt.plot(x, arima.predict(n_periods=test.shape[0]), color='red', alpha=0.5)
+plt.title('Actual test samples vs. forecasts')
+plt.show()"""
+
+"print(arima.summary())"
+
+# Oscilador estocastico
 
 
 
-# Establecer la fecha como indice para que el indice predeterminado (0,1,2,3,4..) no se agregue como una columna nueva
-
-ecopetrol_df = ecopetrol_df.set_index('fecha')
-
-print(ecopetrol_df)
 
 
-ecopetrol_csv_cleaned = ecopetrol_df.to_csv('Ecopetrol limpio.csv', encoding='utf-8')
+
+
+
+"""ecopetrol_csv_cleaned = ecopetrol_df.to_csv('Ecopetrol limpio.csv', encoding='utf-8')
 # Conversión y guardado de los datos en disco
-ecopetrol_csv_limpio = pd.read_csv('Ecopetrol limpio.csv')
-
-print(ecopetrol_csv_limpio)
+ecopetrol_csv_limpio = pd.read_csv('Ecopetrol limpio.csv')"""
